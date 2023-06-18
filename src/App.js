39f5ac8cb53517881ffd125fc1047c60545d1505
Dev-Dords <jrenzodorado@icloud.com>
2023-06-18 20:51:18 +0800

    added routing

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import Homepage from './components/homepage';
 import RegisterPage from './components/register';
 import { NavigationBar } from './components/navigationbar';
 import ScanPage from './components/scan';
+import { Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <main>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/scan" element={<ScanPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </Container>
+      </main>
       {/* <RegisterPage /> */}
       {/* <Homepage /> */}
-      <ScanPage />
+      {/* <ScanPage /> */}
     </div>
   );
 }
--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -1,15 +1,16 @@
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import logo from '../assets/logo.jpg';
+import { Link } from 'react-router-dom';
 export const NavigationBar = () => {
   return (
     <Navbar bg="light" className="border">
       <Container>
-        <Navbar.Brand href="#home">
-            <img src={logo} width="90" height="90" alt="" />
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} width="90" height="90" alt="" />
         </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#register">Register</Nav.Link>
-          <Nav.Link href="#scan">Scan</Nav.Link>
+          <Nav.Link as={Link} to="/register">Register</Nav.Link>
+          <Nav.Link as={Link} to="/scan">Scan</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
