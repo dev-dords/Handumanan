e819a5ff1436e2d4476e923d1513b87e1388468f
Dev-Dords <jrenzodorado@icloud.com>
2023-06-20 09:34:20 +0800

    Styling

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -3,15 +3,11 @@ import logo from '../assets/logo.jpg';
 import { Link } from 'react-router-dom';
 export const NavigationBar = () => {
   return (
-    <Navbar bg="light" className="border">
+    <Navbar >
       <Container>
         <Navbar.Brand as={Link} to="/Handumanan">
           <img src={logo} width="90" height="90" alt="" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/Handumanan/register">
               Register
@@ -20,7 +16,6 @@ export const NavigationBar = () => {
               Scan
             </Nav.Link>
           </Nav>
-        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
