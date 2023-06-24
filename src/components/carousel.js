cd0070c596c7061940558fb182f5cd3c12d4e45d
Dev-Dords <jrenzodorado@icloud.com>
2023-06-24 18:24:22 +0800

    remove carousel as it is slow

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,12 +1,12 @@
 import { Carousel } from 'react-bootstrap';
-import two from '../assets/paskowela/2.jpg';
-import three from '../assets/paskowela/3.jpg';
-import four from '../assets/paskowela/4.jpg';
-import five from '../assets/paskowela/5.jpg';
-import six from '../assets/paskowela/6.jpg';
-import seven from '../assets/paskowela/7.jpg';
-import eight from '../assets/paskowela/8.jpg';
-import ten from '../assets/paskowela/10.jpg';
+// import two from '../assets/paskowela/2.jpg';
+// import three from '../assets/paskowela/3.jpg';
+// import four from '../assets/paskowela/4.jpg';
+// import five from '../assets/paskowela/5.jpg';
+// import six from '../assets/paskowela/6.jpg';
+// import seven from '../assets/paskowela/7.jpg';
+// import eight from '../assets/paskowela/8.jpg';
+// import ten from '../assets/paskowela/10.jpg';
 const CarouselItems = () => {
   return (
     <Carousel>
