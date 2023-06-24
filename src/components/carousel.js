73a953c73d1172e75fddecf11ac8b963efff95a5
Dev-Dords <jrenzodorado@icloud.com>
2023-06-24 19:11:43 +0800

    Slideshow from remote

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,40 +1,26 @@
 import { Carousel } from 'react-bootstrap';
-// import two from '../assets/paskowela/2.jpg';
-// import three from '../assets/paskowela/3.jpg';
-// import four from '../assets/paskowela/4.jpg';
-// import five from '../assets/paskowela/5.jpg';
-// import six from '../assets/paskowela/6.jpg';
-// import seven from '../assets/paskowela/7.jpg';
-// import eight from '../assets/paskowela/8.jpg';
-// import ten from '../assets/paskowela/10.jpg';
 const CarouselItems = () => {
   return (
     <Carousel>
-        {/* <Carousel.Item>
-          <img className="d-block w-100" alt="" src={two} />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" alt="" src={three} />
-        </Carousel.Item>{' '}
-        <Carousel.Item>
-          <img className="d-block w-100" alt="" src={four} />
-        </Carousel.Item>{' '}
-        <Carousel.Item>
-          <img className="d-block w-100" alt="" src={five} />
-        </Carousel.Item>{' '}
-        <Carousel.Item>
-          <img className="d-block w-100" alt="" src={six} />
-        </Carousel.Item>{' '}
-        <Carousel.Item>
-          <img className="d-block w-100" alt="" src={seven} />
-        </Carousel.Item>{' '}
-        <Carousel.Item>
-          <img className="d-block w-100" alt="" src={eight} />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" alt="" src={ten} />
-        </Carousel.Item> */}
-      </Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" alt="" src={'https://anonsharing.com/file/5264b2af153967b5/2.jpg'} />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" alt="" src={'https://anonsharing.com/file/8039559e4f936d66/3.jpg'} />
+      </Carousel.Item>{' '}
+      <Carousel.Item>
+        <img className="d-block w-100" alt="" src={'https://anonsharing.com/file/d06091bfeb81b3b7/4.jpg'} />
+      </Carousel.Item>{' '}
+      <Carousel.Item>
+        <img className="d-block w-100" alt="" src={'https://anonsharing.com/file/e3323b81ced24325/5.jpg'} />
+      </Carousel.Item>{' '}
+      <Carousel.Item>
+        <img className="d-block w-100" alt="" src={'https://anonsharing.com/file/12f0656d042814f6/6.jpg'} />
+      </Carousel.Item>{' '}
+      <Carousel.Item>
+        <img className="d-block w-100" alt="" src={'https://anonsharing.com/file/4ab8a75c15282363/7.jpg'} />
+      </Carousel.Item>{' '}
+    </Carousel>
   );
 };
 export default CarouselItems;
