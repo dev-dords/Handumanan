11019d2cd91e24688a11c1833bd3a3851f4ba2d1
Dev-Dords <jrenzodorado@icloud.com>
2023-06-28 13:32:46 +0800

    Admin List with Dynamic Search, lacks pagination

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Homepage from './components/homepage';
 import RegisterPage from './components/register';
 import { NavigationBar } from './components/navigationbar';
 import ScanPage from './components/scan';
-import { Route, Routes, redirect } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { Payment } from './components/payment';
-import { AdminHome } from './components/adminListView/adminHome';
+// import { AdminHome } from './components/adminListView/adminHome';
 function App() {
   return (
     <div className="App banner">
@@ -20,7 +20,7 @@ function App() {
             <Route path="/Handumanan/scan" element={<ScanPage />} />
             <Route path="/Handumanan/register" element={<RegisterPage />} />
             <Route path="/Handumanan/payment" element={<Payment />} />
-            <Route path="/Handumanan/jrd" element={<AdminHome />} />
+            {/* <Route path="/Handumanan/jrd" element={<AdminHome />} /> */}
           </Routes>
         </Container>
       </main>
